992f4099f90721fcac726221c8beaf16836132c7
Man Mohan Sharma <142076383+17MaNN@users.noreply.github.com>
2026-07-22 21:50:44 +0530

    Refactor fetchFixtures to use BASE URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -10,18 +10,11 @@ export const predictMatch = async (home, away, neutral = 1) => {
 }
 
 export const fetchFixtures = async () => {
-  const res = await axios.get(
-    'https://www.thestatsapi.com/world-cup/data/fixtures.json'
-  )
-  return res.data.fixtures
+  const res = await axios.get(`${BASE}/fixtures`)
+  return res.data
 }
 
 export const fetchAccuracy = async () => {
   const res = await axios.get(`${BASE}/accuracy`)
-  return res.data
-}
-
-export const fetchFixtures = async () => {
-  const res = await axios.get(`${BASE}/fixtures`)
   return res.data
 }
